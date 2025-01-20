b315c108337100e9f76730c5a457db5b808c5810
Chaos <1723117270@qq.com>
2025-01-20 17:50:53 +0800

    Add files via upload

---
+++ pages/index/index.vue
@@ -0,0 +1,176 @@
+<template>
+	<view class="home">
+		<view class="lbt">
+			<!-- <u-swiper :list="list1" @change="change" @click="click"></u-swiper> -->
+			<u-swiper :list="list1" circular showTitle keyName="image" height="200px" indicator
+				indicator-mode="dot"></u-swiper>
+		</view>
+		<view class="search">
+			<u-search :showAction="true" actionText="搜索" placeholder="电脑:Ctrl+F搜佛经名字;手机:工具箱-页内查找" @search="search"
+				:animation="true"></u-search>
+		</view>
+		<view class="titles">
+			<span class="text"> 乾 隆 大 藏 经 </span>
+			<p class="dzj"><a href="/static/qldzj/index.html">龙藏目录</a> </p>
+			<!-- <span class="line"></span> -->
+			<hr />
+			<span class="text">Cbeta</span>
+		</view>
+
+
+		<view class="content">
+			<view class="files">
+				<view class="file" v-for="(item,index) in arr">
+					<b @click="toSee(item.filePath)">{{item.fileName}}</b>
+				</view>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import data from "../../data.json"
+	const decoder = new TextDecoder()
+	export default {
+		components: {
+
+		},
+		data() {
+			return {
+				list1: [{
+					image: '/static/imgs/莲花.jpg',
+					title: '应无所住 而生其心 不取于相 如如不动',
+				}, {
+					image: '/static/imgs/2.jpg',
+					title: '凡所有相 皆是虚妄 知幻即离 离幻即觉'
+				}, {
+					image: '/static/imgs/1.jpg',
+					title: '一翳在眼 空华乱坠 知是空华 即无轮转'
+				}, {
+					image: '/static/imgs/3.jpg',
+					title: '不是息心除妄想 只缘无事可思量'
+				}, {
+					image: '/static/imgs/11.jpg',
+					title: '寂寂断见闻 荡荡心无著|不着见闻 荡然无住'
+				}, {
+					image: '/static/imgs/14.jpg',
+					title: '不惧念起 唯恐觉迟'
+				}, {
+					image: '/static/imgs/6.jpg',
+					title: '外不被境牵 内不随念转'
+				}, {
+					image: '/static/imgs/5.jpg',
+					title: '青山本不动 白云任来去'
+				}],
+				title: "藏经阁",
+				arr: data,
+				path: "../../static/cbeta_epub/"
+			}
+		},
+//吾早年来积学问， 亦曾讨疏寻经论，
+
+//分别名相不知休， 入海算沙徒自困，
+
+//却被如来苦诃责， 数他珍宝有何益？
+
+//从来蹭蹬觉虚行， 多年枉作风尘客。
+
+//——《永嘉证道歌》 ​​​
+		onShow() {
+
+		},
+		onLoad() {
+
+		},
+		methods: {
+			search() {
+				uni.showToast({
+					title: "请直接>电脑:Ctrl+F搜佛经名字;手机:工具箱-页内查找",
+					icon: "none",
+					duration: 3000
+				})
+			},
+
+			toSee(e) {
+				let url = e
+				//本地处理版
+				uni.navigateTo({
+					url: "/pages/read/read?url=" + url
+				})
+				//node接口版
+				// uni.navigateTo({
+				// 	url: "/pages/books/books?url=" + url
+				// })
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	// @font-face {
+	// 	font-family: 'dingliesongtypeface';
+	// 	src: url('../../static/fonts/dingliesongtypeface.ttf');
+	// }
+
+	.home {
+		-webkit-user-select: text;
+		// background-image: url("../../static/imgs/莲花.jpg");
+		background-color: #C8EDC0;
+
+		background-size: 100%;
+
+		.content {
+
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+
+		}
+
+		.lbt {
+			padding: 5rpx;
+		}
+
+		.search {
+			//4上右下左  2上下 ,左右
+			padding: 5rpx 0 20rpx 0;
+		}
+
+
+		.titles {
+			text-align: center;
+			height: 100%;
+		}
+
+		.titles .text {
+			font-size: 20px;
+			font-weight: bold;
+		}
+
+
+		hr {
+			margin-bottom: 30rpx;
+		}
+
+		.titles .dzj {
+			padding: 20rpx;
+			font-size: 20px;
+		}
+
+
+
+		.files {
+
+			.file {
+				height: auto;
+				padding: 10rpx 20rpx;
+				font-variant-east-asian: normal;
+				overflow: hidden;
+				text-overflow: ellipsis;
+
+			}
+		}
+	}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="content">
+		<u-swiper class="uswiper" :height="wHeight" imgMode="aspectFit" :list="list1" duration="500" indicatorMode="dot"
+			indicator="true" indicatorActiveColor="#000000" indicatorInactiveColor="#ffffff" circular :autoplay="true"
+			interval="30000" radius="5" bgColor="#ffffff"></u-swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list1: ['/static/shufa/69.jpg', '/static/shufa/70.jpg', '/static/shufa/37.jpg','/static/shufa/01.webp', '/static/shufa/02.webp', '/static/shufa/03.webp', '/static/shufa/04.webp', '/static/shufa/05.webp', '/static/shufa/06.webp', '/static/shufa/34.jpg', '/static/shufa/35.jpg', '/static/shufa/36.jpg', '/static/shufa/38.jpg', '/static/shufa/39.jpg', '/static/shufa/40.jpg', '/static/shufa/41.jpg', '/static/shufa/42.jpg', '/static/shufa/43.jpg', '/static/shufa/44.jpg', '/static/shufa/45.jpg', '/static/shufa/46.jpg', '/static/shufa/47.jpg', '/static/shufa/48.jpg', '/static/shufa/49.jpg', '/static/shufa/50.jpg', '/static/shufa/51.jpg', '/static/shufa/52.jpg', '/static/shufa/53.jpg', '/static/shufa/54.jpg', '/static/shufa/55.jpg', '/static/shufa/56.jpg', '/static/shufa/57.jpg', '/static/shufa/58.jpg', '/static/shufa/59.jpg', '/static/shufa/60.jpg', '/static/shufa/61.jpg', '/static/shufa/62.jpg', '/static/shufa/63.jpg', '/static/shufa/64.jpg', '/static/shufa/65.jpg', '/static/shufa/66.jpg', '/static/shufa/67.jpg', '/static/shufa/68.jpg',  '/static/shufa/71.jpg', '/static/shufa/72.png', '/static/shufa/73.png', '/static/shufa/74.png', '/static/shufa/75.png', '/static/shufa/76.png', '/static/shufa/77.png', '/static/shufa/78.png', '/static/shufa/79.png', '/static/shufa/80.png', '/static/shufa/81.png', '/static/shufa/82.png', '/static/shufa/83.png', '/static/shufa/84.png', '/static/shufa/85.png', '/static/shufa/86.png', '/static/shufa/87.png', '/static/shufa/88.png', '/static/shufa/89.png', '/static/shufa/90.png', '/static/shufa/91.png','/static/shufa/07.webp', '/static/shufa/08.webp', '/static/shufa/09.webp', '/static/shufa/10.webp', '/static/shufa/11.webp', '/static/shufa/12.webp', '/static/shufa/13.webp', '/static/shufa/14.webp', '/static/shufa/15.webp', '/static/shufa/16.webp', '/static/shufa/17.webp', '/static/shufa/18.webp', '/static/shufa/19.webp', '/static/shufa/20.webp', '/static/shufa/21.webp', '/static/shufa/22.webp', '/static/shufa/23.webp', '/static/shufa/24.webp', '/static/shufa/25.webp', '/static/shufa/26.webp', '/static/shufa/27.webp', '/static/shufa/28.webp', '/static/shufa/29.webp', '/static/shufa/30.webp', '/static/shufa/31.jpg', '/static/shufa/32.jpg', '/static/shufa/33.jpg', ],				wHeight: 0
+			}
+		},
+		onLoad() {
+			const systemInfo = uni.getSystemInfoSync()
+			this.wHeight = systemInfo.windowHeight
+			systemInfo.wind
+			// this.showImgs()
+
+		},
+		methods: {
+			showImgs() {
+				for (var i = 1; i < 92; i++) {
+					console.log(i);
+					if (i < 31) {
+						if (i < 10) {
+							let j = '0' + i
+							this.list1.push('/static/shufa/' + j + '.webp')
+						} else {
+							this.list1.push('/static/shufa/' + i + '.webp')
+						}
+
+					} else if (i>30) {
+						if (i < 72)
+						this.list1.push('/static/shufa/' + i + '.jpg')
+						else{
+							this.list1.push('/static/shufa/' + i + '.png')
+						}
+					} 
+
+				}
+				console.log(this.list1);
+			}
+
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+
+	.uswiper {
+		width: 100%;
+		height: 100%;
+	}
+</style>
